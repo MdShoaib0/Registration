1a519f07ca3bc6dbea7a9f288f7cc1d98bc3a9e8
Muhammed Shoaib <muhammedshoaib06@gmail.com>
2025-09-22 02:01:05 +0530

    Gsap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
-import './App.css'
-import LoginPage from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-import SkillPage from './pages/SkillPage'
-import ProjectPage from './pages/ProjectPage'
-import ServicePage from './pages/ServicePage'
-import ContactPage from './pages/ContactPage'
-import NeonButton from './components/NeonButton'
+import React from 'react';
+import HomePage from './pages/HomePage';
+import SkillPage from './pages/SkillPage';
+import ProjectPage from './pages/ProjectPage';
+import ServicePage from './pages/ServicePage';
+import ContactPage from './pages/ContactPage';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollSmoother } from 'gsap/ScrollSmoother'; // ✅ Requires Club GreenSock
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  useGSAP(() => {
+    // ✅ Creates ScrollSmoother instance
+    const smoother = ScrollSmoother.create({
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+      smooth: 2,
+      effects: true
+    });
+
+    return () => {
+      // ✅ Cleanup on unmount
+      smoother.kill();
+    };
+  });
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-      {/* <LoginPage /> */}
+    <div id="smooth-wrapper">
+      <div id="smooth-content">
         <HomePage />
         <SkillPage />
         <ProjectPage />
         <ServicePage />
         <ContactPage />
-      {/* <NeonButton/> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
