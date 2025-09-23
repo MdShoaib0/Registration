8e2af4b2364417f21b6c63495d181fc9d3c3132b
Muhammed Shoaib <muhammedshoaib06@gmail.com>
2025-09-23 22:13:08 +0530

    Make Code Saprate

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import React from 'react';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
-import Header from './components/Header';
+import React from 'react'
+import LoginPage from './components/LoginPage'
 
 function App() {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-  useGSAP(() => {
-    const smoother = ScrollSmoother.create({
-      wrapper: '#smooth-wrapper',
-      content: '#smooth-content',
-      smooth: 3,
-      effects: true
-    });
-
-    return () => {
-      smoother.kill();
-    };
-  });
-
   return (
-    <div id="smooth-wrapper">
-      <div id="smooth-content">
-        <Header />
-      </div>
-    </div>
-  );
+    <>
+    <LoginPage/>
+    </>
+  )
 }
 
-export default App;
+export default App
